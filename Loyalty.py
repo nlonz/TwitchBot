0246707326c03e1be04def3a786a5982e8d8a4fe
Nick Lonsway <nlonz@yahoo.com>
2017-01-04 22:47:42 -0600

    Catch HTTP 503 error from Twitch and fix double points and gamble probabilities

--- Loyalty.py
+++ Loyalty.py
@@ -2,12 +2,17 @@ import urllib2, json, datetime
 from Config import CHANNEL
 from Gamble import writePointTotals, retrievePointTotals
 
+added = False
+
 def getUserList():
-	url = "http://tmi.twitch.tv/group/user/" + CHANNEL + "/chatters"
-	return json.loads(urllib2.urlopen(url).read().decode("utf-8")).get("chatters")
+	try:
+		url = "http://tmi.twitch.tv/group/user/" + CHANNEL + "/chatters"
+		return json.loads(urllib2.urlopen(url).read().decode("utf-8")).get("chatters")
+	except urllib2.HTTPError as err:
+		if err.code == 503:
+			getUserList()
 
 def updateTotals(user, pointTotals):
-	print user
 	points = pointTotals.get(user.upper(), 0)
 	pointTotals[user.upper()] = (points + 5)
 	
@@ -41,10 +46,8 @@ def addLoyaltyForUser(user):
 	
 while True:
 	currentTime = datetime.datetime.now()
-	added = False
 	if 0 == currentTime.second and not added:
 		addLoyalty()
-		print retrievePointTotals()
 		added = True
 	else:
 		added = False
